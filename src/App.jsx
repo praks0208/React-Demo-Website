c0240f58b7d4c7fd6a8b0c18ae32c01a759be37c
praks0208 <praks.sawant26@gmail.com>
2024-05-19 16:31:01 +0530

    React to Events

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
@@ -28,7 +29,15 @@ function App() {
             />
           </ul>
         </section>
-
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
