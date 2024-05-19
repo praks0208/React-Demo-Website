38a7d3c10a4a073a902afa531eeaef12e8ee5c16
praks0208 <praks.sawant26@gmail.com>
2024-05-19 15:11:05 +0530

    Use  Alternative Props Syntaxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png"
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -22,11 +22,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props){
+// Using object destructuring instead of props parameter
+function CoreConcept({image,title,description}) {
   return <li>
-    <img src={props.image} alt={props.title} />
-    <h3>{props.title}</h3>
-    <p>{props.description}</p>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
   </li>
 }
 
@@ -36,19 +37,25 @@ function App() {
       <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept 
-          title="Components" 
-          description="The core UI buiding block."
-          image={componentsImg}
-          />
-          <CoreConcept title="Props" />
-          <CoreConcept />
-          <CoreConcept />
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+             {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcept
+         {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcept
+           {...CORE_CONCEPTS[3]}
+            />
+          </ul>
         </section>
-   
+
       </main>
     </div>
   );
