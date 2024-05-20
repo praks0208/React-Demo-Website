b27b943f6b25c83cd428c45f1a2953273428ff76
praks0208 <praks.sawant26@gmail.com>
2024-05-20 21:00:25 +0530

    Set Component Types Dynamically

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,6 +1,7 @@
-export default function Tabs({children, buttons}){
-    return <>
-    <menu>{buttons}</menu>
+export default function Tabs({children, buttons, ButtonsContainer}){
+//    const ButtonsContainer = buttonsContainer
+   return <>
+    <ButtonsContainer>{buttons}</ButtonsContainer>
     {children}
     </>
 }
